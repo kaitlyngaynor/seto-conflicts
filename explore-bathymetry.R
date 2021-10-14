e6e74a454e14273317b430325aaed3f671017872
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2021-10-14 12:51:34 -0700

    Compare bathymetry layer to 30mContourpoly

--- explore-bathymetry.R
+++ explore-bathymetry.R
@@ -1,5 +1,8 @@
 library(raster)
 library(mapview)
+library(dplyr)
+library(sf)
+library(ggplot2)
 
 setwd("~/Dropbox/AAS GIS")
 
@@ -17,3 +20,20 @@ plot(bathy_75)
 bathy_30 <- bathy
 bathy_30[bathy_30 < -30] <- NA
 plot(bathy_30)
+plot(bathy_30, col = "grey")
+
+bathy_30_poly <- rasterToPolygons(bathy_30)
+
+# does this look like the shapefile?
+contour_30 <- st_read("Kelly Data/30mContourpoly.shp") %>% 
+    st_transform(crs = "+proj=utm +zone=30 +datum=WGS84 +units=m +no_defs")
+plot(contour_30)
+
+mapview(bathy_30) + mapview(contour_30)
+
+ggplot() +
+    geom_sf(data = contour_30,
+            fill = "grey", color = "grey", alpha = 1) +
+    theme_minimal() 
+
+# no it doesn't
